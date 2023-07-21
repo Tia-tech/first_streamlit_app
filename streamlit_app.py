6ac988887142fb02e7c760a2cae3067a8afa14cb
Tia-tech <64370113+Tia-tech@users.noreply.github.com>
2023-07-21 07:48:52 +0200

    Update streamlit_app.py
    Add second header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('🥣 Breakfast Menu')
 streamlit.text('🥗 Omega 3 & Blueabarry Oatmeal')
 streamlit.text('🐔 Kale & Spinach, Rocket Smoothie')
 streamlit.text('🥑🍞 Hard Bioled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
