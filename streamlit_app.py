935e35338f0fb9f6074771be4f36737a51c0a439
Tia-tech <64370113+Tia-tech@users.noreply.github.com>
2023-08-01 09:33:37 +0200

    Update streamlit_app.py
    fixinge errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
 
 streamlit.title('My parents New Healthy Diner')
 
@@ -9,52 +12,30 @@ streamlit.text('🥑🍞 Hard Bioled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-# Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi', key='fruit_info_input')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-
-
-# what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
-
-
-import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
 # Adding a second text box for the new fruit
-def add_my_fruit():
-    new_fruit = streamlit.text_input('Enter a new fruit to add to the list', '')
-    return new_fruit
-
-new_fruit = add_my_fruit()
+new_fruit = streamlit.text_input('Enter a new fruit to add to the list', '', key='new_fruit_input')
 if new_fruit:
-    my_data_rows.append((new_fruit,))  # append new fruit to the fetched data list
+    my_data_rows.append((new_fruit,))
 
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
